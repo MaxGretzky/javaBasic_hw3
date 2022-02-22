fdd9d032def0b814257928a4ed0ca3ccba6e183e
Max Gretzky <maxim.gretzky@ringteam.com>
2022-02-23 00:45:42 +0200

    seasons added

--- src/SeasonExample.java
+++ src/SeasonExample.java
@@ -1,6 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.System.out;
+
 public class SeasonExample {
+
+    public enum Season {
+        AUTUMN, SPRING, SUMMER, WINTER
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Select any month");
+        String month = br.readLine();
+        Season season;
+        switch (month) {
+            case "December":
+            case "January":
+            case "February":
+                season = Season.WINTER;
+                System.out.println(season);
+                break;
+            case "March":
+            case "April":
+            case "May":
+                season = Season.SPRING;
+                System.out.println(season);
+                break;
+            case "June":
+            case "July":
+            case "August":
+                season = Season.SUMMER;
+                System.out.println(season);
+                break;
+            case "September":
+            case "October":
+            case "November":
+                season = Season.AUTUMN;
+                System.out.println(season);
+                break;
+            default:
+                System.out.println("No such the month");
+        }
+    }
 }
-
-enum Season {WINTER, SPRING, SUMMER, AUTUMN}
-
-xzczxcm ,
