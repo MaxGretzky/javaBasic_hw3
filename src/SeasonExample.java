a99d9cdd81ecacea98f311e7e7deb73626a67d48
Max Gretzky <maxim.gretzky@ringteam.com>
2022-02-23 00:51:02 +0200

    code done

--- src/SeasonExample.java
+++ src/SeasonExample.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
+
 import static java.lang.System.out;
 
 public class SeasonExample {
@@ -14,22 +16,22 @@ public class SeasonExample {
         System.out.println("Select any month");
         String month = br.readLine();
         Season season;
-        switch (month) {
-            case "December":
-            case "January":
-            case "February":
+        switch (month.toLowerCase(Locale.ROOT)) {
+            case "december":
+            case "january":
+            case "february":
                 season = Season.WINTER;
                 System.out.println(season);
                 break;
-            case "March":
-            case "April":
-            case "May":
+            case "march":
+            case "april":
+            case "may":
                 season = Season.SPRING;
                 System.out.println(season);
                 break;
-            case "June":
-            case "July":
-            case "August":
+            case "june":
+            case "july":
+            case "august":
                 season = Season.SUMMER;
                 System.out.println(season);
                 break;
